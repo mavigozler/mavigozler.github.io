d4cd06e80100b56e2576cdd8e4dea531a304b14f
TSMitch <stevepenner1960@gmail.com>
2024-12-09 11:13:53 -0800

    final responsive coding and styling

---
+++ setupDist.ps1
@@ -0,0 +1,18 @@
+﻿$sourceFiles = @(
+   "index.html",
+   "posting.css"
+#   "tutor-posting.ts"
+)
+$sourcePath = "src"
+$destPath = "dist"
+foreach ($item in $sourceFiles) {
+   $srcInfo = Get-ChildItem -Path "${sourcePath}/${item}"
+   if (Test-Path -Path "${destPath}/${item}") {
+      $destInfo = Get-ChildItem -Path "${destPath}/${item}"
+   } else {
+      $destInfo = $null
+   }
+   if ($null -eq $destInfo -or $srcInfo.LastWriteTime.Equals($destInfo.LastWriteTime) -eq $False) {
+      Copy-Item -Path "${sourcePath}/${item}" -Destination "${destPath}/${item}"
+   }
+}
